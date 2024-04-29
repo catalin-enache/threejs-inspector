3aee4602fe87302ad181f18b1c462d8b419b9fc8
Catalin Enache <catalinenache78@gmail.com>
2024-04-29 11:58:27 +0300

    updated vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,12 @@ import { Mode, plugin as mdPlugin } from 'vite-plugin-markdown';
 import path from 'path';
 
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mdPlugin({ mode: [Mode.HTML, Mode.MARKDOWN] })],
   root: 'src/',
+  base: process.env.NODE_ENV === 'production' ? '/threejs-inspector/' : './',
   publicDir: '../public',
-  base: './',
   server: {
     host: true,
     open: !isCodeSandbox

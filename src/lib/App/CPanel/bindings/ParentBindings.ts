a19d6245615eae5992eae4b2106a03c610d7d7a0
Catalin Enache <catalinenache78@gmail.com>
2024-05-13 12:24:17 +0300

    moved parent bindings into bindingsHelper to avoid infinite loops

---
+++ src/lib/App/CPanel/bindings/ParentBindings.ts
@@ -0,0 +1,28 @@
+import type { CommonGetterParams } from './bindingTypes';
+
+export const ParentBindings = (_params: CommonGetterParams) => ({
+  parent: {
+    // for Object3D
+    // title: 'Parent',
+    id: {
+      label: 'ID',
+      view: 'text',
+      disabled: true
+    },
+    uuid: {
+      label: 'UUID',
+      view: 'text',
+      disabled: true
+    },
+    name: {
+      label: 'Name',
+      view: 'text',
+      disabled: true
+    },
+    type: {
+      label: 'Type',
+      view: 'text',
+      disabled: true
+    }
+  }
+});

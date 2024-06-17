cc4fb0b00b9969f95fa92e91f468791c92b18fa7
Catalin Enache <catalinenache78@gmail.com>
2024-06-17 12:06:19 +0300

    updated CustomControl

--- src/components/CustomControl/CustomControl.ts
+++ src/components/CustomControl/CustomControl.ts
@@ -1,89 +1,36 @@
-import { useEffect, useRef, memo } from 'react';
 import { BindingParams } from 'tweakpane';
 import { useAppStore } from 'src/store';
-import { isEqual, shallowClone } from 'lib/utils';
+import { useEffect } from 'react';
 
 interface CustomControlProps {
   name: string;
+  object: any;
+  prop: string;
   control: BindingParams;
-  value: any;
-  onChange?: (value: any) => void;
+  path?: string;
 }
 
-/*
-The props that CustomControl reacts to are "value" and "onChange".
-"name" and control are read only once at initialization time.
-Old value and new value are compared using shallow compare.
-The old value is cached in cachedValueRef.
-Flow:
- - cPanel updates a binding for custom param in place (mutation)
-    and calls triggerCPanelCustomParamsChanged which changes cPanelCustomParamsStateFake
- - we react to cPanelCustomParamsStateFake and call onChange
- - the app can use the value reported in onChange to make other updates including updating other CustomControls
- - due to this the app will also send back the new value to the CustomControl (controlled component) that called onChange
-   but since it is the same value the component will do nothing.
- - if the value changes from the app, the component will update the store by calling setCPanelCustomParams
-*/
-export const CustomControl = memo((props: CustomControlProps) => {
-  const { onChange, value } = props;
-  const onChangeRef = useRef(onChange);
-  onChangeRef.current = onChange;
-  const cPanelCustomParams = useAppStore((state) => state.getCPanelCustomParams());
-  const cachedValueRef = useRef(null);
-  const removeCPanelCustomParams = useAppStore((state) => state.removeCPanelCustomParams);
-  const cPanelCustomParamsStateFake = useAppStore((state) => state.cPanelCustomParamsStateFake);
-  const setCPanelCustomControls = useAppStore((state) => state.setCPanelCustomControls);
-  const setOrUpdateCPanelCustomParams = useAppStore((state) => state.setOrUpdateCPanelCustomParams);
-  const removeCPanelCustomControls = useAppStore((state) => state.removeCPanelCustomControls);
+export const CustomControl = (props: CustomControlProps) => {
+  const { name, prop, control, path = '', object } = props;
+  const setOrUpdateCPanelCustomParams_2 = useAppStore((state) => state.setOrUpdateCPanelCustomParams);
+  const removeCPanelCustomParams_2 = useAppStore((state) => state.removeCPanelCustomParams);
+  const triggerCPanelCustomParamsStructureChanged_2 = useAppStore(
+    (state) => state.triggerCPanelCustomParamsStructureChanged
+  );
 
-  // Read from store and call onChange
-  // cPanelCustomParamsStateFake notifies for every change in cPanelCustomParams
-  // (done in cPanel by observing the bindings to cPanelCustomParams)
-  // Note: we need deep comparison here since .current is a copy (always different).
-  // The copy is needed because Tweakpane updates params in place
+  // console.log('CustomControl_2', name);
   useEffect(() => {
-    const valueFromPanel = cPanelCustomParams[props.name];
-    if (
-      valueFromPanel !== undefined &&
-      !isEqual(cachedValueRef.current, valueFromPanel) &&
-      !isEqual(valueFromPanel, value)
-    ) {
-      // Note: Tweakpane updates params in place.
-      // We need to make a copy so that cachedValueRef is not mutated
-      // and also react state to not get mutated.
-      const clonedValue = shallowClone(valueFromPanel);
-      cachedValueRef.current = clonedValue;
-      onChangeRef.current?.(clonedValue); // with a clone setState detects changes
-    }
-  }, [value, cPanelCustomParamsStateFake]);
-
-  // Read from value and set on store
-  useEffect(() => {
-    if (!isEqual(cachedValueRef.current, value) && value !== undefined) {
-      const clonedValue = shallowClone(value);
-      cachedValueRef.current = clonedValue;
-      // setOrUpdateCPanelCustomParams does not replace objects but mutates them
-      // in order to not make Tweakpane control stale objects.
-      setOrUpdateCPanelCustomParams(props.name, clonedValue);
-      // useAppStore.getState().triggerCPanelCustomParamsChanged() is not needed.
-      // CustomControl is interested in its own value.
-      // Other controls can be changed by reacting to onChange from this control.
-    }
-  }, [value]);
-
-  useEffect(() => {
-    // using shallowClone so that cPanel won't mutate React state value
-    const clonedValue = shallowClone(props.value);
-    setOrUpdateCPanelCustomParams(props.name, clonedValue);
-    setCPanelCustomControls(props.name, props.control);
-    // useAppStore.getState().triggerCPanelCustomParamsChanged();
+    const pathArray = path
+      .split('/')
+      .map((p) => p.trim())
+      .filter((p) => p.length > 0);
+    setOrUpdateCPanelCustomParams_2(name, object, prop, control, pathArray);
+    triggerCPanelCustomParamsStructureChanged_2();
     return () => {
-      removeCPanelCustomParams(props.name);
-      removeCPanelCustomControls(props.name);
-      // useAppStore.getState().triggerCPanelCustomParamsChanged();
+      removeCPanelCustomParams_2(name, pathArray);
+      triggerCPanelCustomParamsStructureChanged_2();
     };
-    // name and control are read only once
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- we want these to run once
   }, []);
+
   return null;
-}, isEqual);
+};

a03536dda3fecd8cc87cd4a58cc953e796515d19
Catalin Enache <catalinenache78@gmail.com>
2024-06-19 10:43:27 +0300

    updated __inspectorData type

--- src/types.ts
+++ src/types.ts
@@ -1,31 +1,4 @@
 import * as THREE from 'three';
-import type { LightProbeHelper } from 'three/examples/jsm/helpers/LightProbeHelper';
-import type { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper';
-
-export type __inspectorData = {
-  fullData: any;
-  animations: THREE.AnimationClip[];
-  hitRedirect: THREE.Object3D;
-  picker: THREE.Mesh;
-  helper:
-    | THREE.SkeletonHelper
-    | THREE.SpotLightHelper
-    | THREE.CameraHelper
-    | RectAreaLightHelper
-    | THREE.DirectionalLightHelper
-    | THREE.PointLightHelper
-    | THREE.HemisphereLightHelper
-    | LightProbeHelper
-    | THREE.Mesh; // fallback meaningless helper
-  isInspectable: boolean;
-  useOnPlay: boolean;
-  isPicker: boolean;
-  defaultPerspectiveCamera: THREE.PerspectiveCamera;
-  defaultOrthographicCamera: THREE.OrthographicCamera;
-  currentCamera: THREE.Camera;
-  cameraToUseOnPlay: THREE.Camera;
-  isRecombined: boolean;
-};
 
 export type BufferAttributeConstructor = new (
   array: ArrayLike<number>,
--- tsExtensions.ts
+++ tsExtensions.ts
@@ -1,8 +1,37 @@
 // three-extensions.d.ts
 // @ts-ignore
 import * as THREE from 'three';
+import type { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper';
+import type { LightProbeHelper } from 'three/examples/jsm/helpers/LightProbeHelper';
+import type { TransformControls } from 'three/examples/jsm/controls/TransformControls';
+import type { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+type __inspectorData = {
+  fullData: any;
+  animations: THREE.AnimationClip[];
+  hitRedirect: THREE.Object3D;
+  picker: THREE.Mesh;
+  helper:
+    | THREE.SkeletonHelper
+    | THREE.SpotLightHelper
+    | THREE.CameraHelper
+    | RectAreaLightHelper
+    | THREE.DirectionalLightHelper
+    | THREE.PointLightHelper
+    | THREE.HemisphereLightHelper
+    | LightProbeHelper
+    | THREE.Mesh; // fallback meaningless helper
+  isInspectable: boolean;
+  useOnPlay: boolean;
+  isPicker: boolean;
+  currentCamera: THREE.PerspectiveCamera | THREE.OrthographicCamera;
+  isRecombined: boolean;
+  transformControlsRef: { current?: TransformControls | null };
+  orbitControlsRef: { current?: OrbitControls | null };
+};
+
 declare module 'three' {
   export interface Object3D {
-    __inspectorData: Record<string, any>; // Define __inspectorData as an optional property of any type
+    __inspectorData: __inspectorData; // Define __inspectorData as an optional property of any type
   }
 }

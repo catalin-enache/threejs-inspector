246ff84b2a74a6e31294f6482aff95fb0d9344f1
Catalin Enache <catalinenache78@gmail.com>
2024-08-05 15:28:13 +0300

    added FBXLoader test

---
+++ src/tests/loaders/FBXLoader/FBXLoader.test.ts
@@ -0,0 +1,34 @@
+import * as THREE from 'three';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
+import { withScene } from '../../testutils/testScene';
+import { expect, describe, it } from 'vitest';
+
+describe('FBXLoader', () => {
+  it('geometry.attributes.position.count === geometry.morphAttributes.position[0].count', () =>
+    new Promise<void>((done, rej) => {
+      const fbxLoader = new FBXLoader();
+      let baseGeometryLength, morphAttributesLength;
+      fbxLoader.load(
+        '/models/MyTests/morph_test/morph_test.fbx',
+        (fbx) => {
+          const mesh = fbx.children[0] as THREE.Mesh;
+          console.log('mesh', mesh);
+          mesh.castShadow = true;
+          withScene(0)(({ scene }) => {
+            baseGeometryLength = mesh.geometry.attributes.position.count;
+            morphAttributesLength = mesh.geometry.morphAttributes.position[0].count;
+            expect(baseGeometryLength).to.equal(morphAttributesLength);
+            scene.add(fbx);
+            return () => {
+              done();
+              scene.remove(fbx);
+            };
+          });
+        },
+        () => {},
+        () => {
+          rej();
+        }
+      );
+    }));
+});

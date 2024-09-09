cd5fed7d9de8cf11623257a501947b47eae363b9
Catalin Enache <catalinenache78@gmail.com>
2024-09-09 10:18:32 +0300

    added sceneUtils.ts

---
+++ src/lib/utils/sceneUtils.ts
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+
+export function disposeMaterial(material: THREE.Material) {
+  for (const key in material) {
+    // @ts-ignore
+    const value = material[key] ?? {};
+    if (value instanceof THREE.Texture) {
+      value.dispose();
+    }
+  }
+  // Finally, dispose the material itself
+  material.dispose();
+}
+
+export function deepCleanScene(scene: THREE.Scene) {
+  scene.traverse((object: any) => {
+    if (object.geometry) {
+      object.geometry.dispose();
+    }
+
+    if (object.material) {
+      if (Array.isArray(object.material)) {
+        object.material.forEach((material: THREE.Material) => {
+          disposeMaterial(material);
+        });
+      } else {
+        disposeMaterial(object.material);
+      }
+    }
+    if ('dispose' in object) {
+      object.dispose();
+    }
+  });
+
+  // Remove all children from the scene
+  while (scene.children.length > 0) {
+    scene.remove(scene.children[0]);
+  }
+}

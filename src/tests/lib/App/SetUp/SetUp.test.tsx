f2c176b6c162402f30bb393f9d19fc800c127e3a
Catalin Enache <catalinenache78@gmail.com>
2024-11-20 17:56:09 +0200

    added new OrbitControl test

--- src/tests/lib/App/SetUp/SetUp.test.tsx
+++ src/tests/lib/App/SetUp/SetUp.test.tsx
@@ -5,6 +5,7 @@ import { defaultScene, defaultPerspectiveCamera } from 'lib/App/SetUp/patchThree
 import { OrbitControls as InternalOrbitControls } from 'lib/third_party/OrbitControls';
 import { OrbitControls as OrbitControlsImpl } from 'three-stdlib';
 import { SETUP_EFFECT, SetUpProps } from 'lib/App/SetUp/SetUp';
+import { useAppStore } from 'src/store';
 
 describe('SetUp', () => {
   beforeEach(() => {
@@ -15,133 +16,160 @@ describe('SetUp', () => {
     clearDOM();
   });
 
-  it('Can be used with defaultScene and default(Perspective/Orthographic)Camera', { timeout: 1000 }, async () => {
-    return new Promise<void>((done) => {
-      const handleThreeChange: SetUpProps['onThreeChange'] = (changed, three) => {
-        if (changed === 'scene') {
-          const { scene } = three;
-          expect(scene).toBe(defaultScene);
-          expect(scene.__inspectorData.currentCamera).toBe(defaultPerspectiveCamera);
-          res.unmount();
-          done();
-        }
-      };
-      const res = render(
-        <TestApp
-          useDreiOrbitControls={false}
-          onThreeChange={handleThreeChange}
-          useDefaultPerspectiveCamera={true}
-          useDefaultScene={true}
-        ></TestApp>,
-        {
-          container: document.getElementById('main')!
-        }
-      );
-    });
-  });
-
-  it('Can be used with custom scene and camera', { timeout: 1000 }, async () => {
-    return new Promise<void>((done) => {
-      const handleThreeChange: SetUpProps['onThreeChange'] = (changed, three) => {
-        if (changed === 'scene') {
-          const { scene } = three;
-          expect(scene).not.toBe(defaultScene);
-          expect(scene.__inspectorData.currentCamera).not.toBe(defaultPerspectiveCamera);
-          res.unmount();
-          done();
-        }
-      };
-      const res = render(<TestApp useDreiOrbitControls={false} onThreeChange={handleThreeChange}></TestApp>, {
-        container: document.getElementById('main')!
+  describe('OrbitControls', () => {
+    it('Can be used with defaultScene and default(Perspective/Orthographic)Camera', { timeout: 1000 }, async () => {
+      return new Promise<void>((done) => {
+        const handleThreeChange: SetUpProps['onThreeChange'] = (changed, three) => {
+          if (changed === 'scene') {
+            const { scene } = three;
+            expect(scene).toBe(defaultScene);
+            expect(scene.__inspectorData.currentCamera).toBe(defaultPerspectiveCamera);
+            res.unmount();
+            done();
+          }
+        };
+        const res = render(
+          <TestApp
+            useDreiOrbitControls={false}
+            onThreeChange={handleThreeChange}
+            useDefaultPerspectiveCamera={true}
+            useDefaultScene={true}
+          ></TestApp>,
+          {
+            container: document.getElementById('main')!
+          }
+        );
       });
     });
-  });
 
-  it('Does not require OrbitControls', { timeout: 1000 }, async () => {
-    return new Promise<void>((done) => {
-      const handleSetupEffect: SetUpProps['onSetupEffect'] = (effect, data) => {
-        if (effect === SETUP_EFFECT.ORBIT_CONTROLS) {
-          expect(data.orbitControlsInUse).toBe(null);
-          res.unmount();
-          done();
-        }
-      };
-
-      const res = render(
-        <TestApp useDreiOrbitControls={false} autoNavControls={false} onSetupEffect={handleSetupEffect}></TestApp>,
-        {
+    it('Can be used with custom scene and camera', { timeout: 1000 }, async () => {
+      return new Promise<void>((done) => {
+        const handleThreeChange: SetUpProps['onThreeChange'] = (changed, three) => {
+          if (changed === 'scene') {
+            const { scene } = three;
+            expect(scene).not.toBe(defaultScene);
+            expect(scene.__inspectorData.currentCamera).not.toBe(defaultPerspectiveCamera);
+            res.unmount();
+            done();
+          }
+        };
+        const res = render(<TestApp useDreiOrbitControls={false} onThreeChange={handleThreeChange}></TestApp>, {
           container: document.getElementById('main')!
-        }
-      );
+        });
+      });
     });
-  });
 
-  it('Can use internal OrbitControls when autoNavControls is true', { timeout: 1000 }, async () => {
-    return new Promise<void>((done) => {
-      const handleSetupEffect: SetUpProps['onSetupEffect'] = (effect, data) => {
-        if (effect === SETUP_EFFECT.ORBIT_CONTROLS) {
-          expect(data.orbitControlsInUse).toBeInstanceOf(InternalOrbitControls);
-          res.unmount();
-          done();
-        }
-      };
-
-      const res = render(
-        <TestApp useDreiOrbitControls={false} autoNavControls={true} onSetupEffect={handleSetupEffect}></TestApp>,
-        {
-          container: document.getElementById('main')!
-        }
-      );
-    });
-  });
-
-  it('Can be used with external OrbitControls', { timeout: 1000 }, async () => {
-    return new Promise<void>((done) => {
-      let calls = 0;
-      const handleSetupEffect: SetUpProps['onSetupEffect'] = (effect, data) => {
-        if (effect === SETUP_EFFECT.ORBIT_CONTROLS) {
-          if (calls === 0) {
+    it('Does not require OrbitControls', { timeout: 1000 }, async () => {
+      return new Promise<void>((done) => {
+        const handleSetupEffect: SetUpProps['onSetupEffect'] = (effect, data) => {
+          if (effect === SETUP_EFFECT.ORBIT_CONTROLS) {
             expect(data.orbitControlsInUse).toBe(null);
-          } else if (calls === 1) {
-            expect(data.orbitControlsInUse).toBeInstanceOf(OrbitControlsImpl);
             res.unmount();
             done();
           }
-          calls += 1;
-        }
-      };
+        };
 
-      const res = render(
-        <TestApp useDreiOrbitControls={true} autoNavControls={false} onSetupEffect={handleSetupEffect}></TestApp>,
-        {
-          container: document.getElementById('main')!
-        }
-      );
+        const res = render(
+          <TestApp useDreiOrbitControls={false} autoNavControls={false} onSetupEffect={handleSetupEffect}></TestApp>,
+          {
+            container: document.getElementById('main')!
+          }
+        );
+      });
     });
-  });
 
-  it('Can be used with external OrbitControls even when autoNavControls is true', { timeout: 1000 }, async () => {
-    return new Promise<void>((done) => {
-      let calls = 0;
-      const handleSetupEffect: SetUpProps['onSetupEffect'] = (effect, data) => {
-        if (effect === SETUP_EFFECT.ORBIT_CONTROLS) {
-          if (calls === 0) {
+    it('Can use internal OrbitControls when autoNavControls is true', { timeout: 1000 }, async () => {
+      return new Promise<void>((done) => {
+        const handleSetupEffect: SetUpProps['onSetupEffect'] = (effect, data) => {
+          if (effect === SETUP_EFFECT.ORBIT_CONTROLS) {
             expect(data.orbitControlsInUse).toBeInstanceOf(InternalOrbitControls);
-          } else if (calls === 1) {
-            expect(data.orbitControlsInUse).toBeInstanceOf(OrbitControlsImpl);
             res.unmount();
             done();
           }
-          calls += 1;
-        }
-      };
+        };
 
-      const res = render(
-        <TestApp useDreiOrbitControls={true} autoNavControls={true} onSetupEffect={handleSetupEffect}></TestApp>,
-        {
-          container: document.getElementById('main')!
-        }
-      );
+        const res = render(
+          <TestApp useDreiOrbitControls={false} autoNavControls={true} onSetupEffect={handleSetupEffect}></TestApp>,
+          {
+            container: document.getElementById('main')!
+          }
+        );
+      });
+    });
+
+    it('Can be used with external OrbitControls', { timeout: 1000 }, async () => {
+      return new Promise<void>((done) => {
+        let calls = 0;
+        const handleSetupEffect: SetUpProps['onSetupEffect'] = (effect, data) => {
+          if (effect === SETUP_EFFECT.ORBIT_CONTROLS) {
+            if (calls === 0) {
+              expect(data.orbitControlsInUse).toBe(null);
+            } else if (calls === 1) {
+              expect(data.orbitControlsInUse).toBeInstanceOf(OrbitControlsImpl);
+              res.unmount();
+              done();
+            }
+            calls += 1;
+          }
+        };
+
+        const res = render(
+          <TestApp useDreiOrbitControls={true} autoNavControls={false} onSetupEffect={handleSetupEffect}></TestApp>,
+          {
+            container: document.getElementById('main')!
+          }
+        );
+      });
+    });
+
+    it('Can be used with external OrbitControls even when autoNavControls is true', { timeout: 1000 }, async () => {
+      return new Promise<void>((done) => {
+        let calls = 0;
+        const handleSetupEffect: SetUpProps['onSetupEffect'] = (effect, data) => {
+          if (effect === SETUP_EFFECT.ORBIT_CONTROLS) {
+            if (calls === 0) {
+              expect(data.orbitControlsInUse).toBeInstanceOf(InternalOrbitControls);
+            } else if (calls === 1) {
+              expect(data.orbitControlsInUse).toBeInstanceOf(OrbitControlsImpl);
+              res.unmount();
+              done();
+            }
+            calls += 1;
+          }
+        };
+
+        const res = render(
+          <TestApp useDreiOrbitControls={true} autoNavControls={true} onSetupEffect={handleSetupEffect}></TestApp>,
+          {
+            container: document.getElementById('main')!
+          }
+        );
+      });
+    });
+
+    it('OrbitControls are disabled when cameraControl !== `orbit', { timeout: 1000 }, async () => {
+      return new Promise<void>((done) => {
+        let calls = 0;
+        const handleSetupEffect: SetUpProps['onSetupEffect'] = async (effect, data) => {
+          if (effect === SETUP_EFFECT.ORBIT_CONTROLS) {
+            if (calls === 0) {
+              expect(data.orbitControlsInUse).toBeInstanceOf(InternalOrbitControls);
+              expect(data.orbitControlsInUse.enabled).toBe(false);
+              res.unmount();
+              done();
+            }
+            calls += 1;
+          }
+        };
+
+        useAppStore.getState().setCameraControl('fly');
+        const res = render(
+          <TestApp useDreiOrbitControls={false} autoNavControls={true} onSetupEffect={handleSetupEffect}></TestApp>,
+          {
+            container: document.getElementById('main')!
+          }
+        );
+      });
     });
   });
 });

0cb44e3428d85dd2fa49bbd8c4126a84500eae6e
Catalin Enache <catalinenache78@gmail.com>
2024-11-25 17:42:09 +0200

    updated customParamsNative test

--- src/tests/customParams/customParamsNative.test.ts
+++ src/tests/customParams/customParamsNative.test.ts
@@ -14,44 +14,47 @@ describe('Custom Params', () => {
     useAppStore.getState().reset();
   });
 
-  it('can be changed at runtime and have nested structure', { timeout: 60000 }, async () => {
+  it('can be changed at runtime and have nested structure', { timeout: 1000 }, async () => {
     return new Promise<void>((done) => {
       const spyOnRemoveCPanelCustomParams = vi.spyOn(useAppStore.getState(), 'removeCPanelCustomParams');
       const spyOnSetOrUpdateCPanelCustomParams = vi.spyOn(useAppStore.getState(), 'setOrUpdateCPanelCustomParams');
 
       // prettier-ignore
-      const obj = { val1: 0, val2: 0 };
+      const selectObject = { asset: 'Samba Dancing' };
+      const morphObject = { val1: 0, val2: 0 };
 
       const handleValueChange = vi.fn();
 
+      const customParamsMorph = {
+        nested: {
+          level: {
+            key_1: {
+              object: morphObject,
+              prop: 'val1',
+              control: {
+                label: 'Key 1',
+                min: 0,
+                max: 1,
+                onChange: handleValueChange
+              } as BindingParams
+            },
+            key_2: {
+              object: morphObject,
+              prop: 'val2',
+              control: {
+                label: 'Key 2',
+                min: 0,
+                max: 1,
+                onChange: handleValueChange
+              } as BindingParams
+            }
+          }
+        }
+      };
+
       const handleCParamChange = (value: string) => {
         if (value === 'morph_test') {
-          customParams.morph = {
-            nested: {
-              level: {
-                key_1: {
-                  object: obj,
-                  prop: 'val1',
-                  control: {
-                    label: 'Key 1',
-                    min: 0,
-                    max: 1,
-                    onChange: handleValueChange
-                  } as BindingParams
-                },
-                key_2: {
-                  object: obj,
-                  prop: 'val2',
-                  control: {
-                    label: 'Key 2',
-                    min: 0,
-                    max: 1,
-                    onChange: handleValueChange
-                  } as BindingParams
-                }
-              }
-            }
-          };
+          customParams.morph = customParamsMorph;
         } else {
           delete customParams.morph;
         }
@@ -60,9 +63,7 @@ describe('Custom Params', () => {
 
       const customParams: NonNullable<InitNativeAppProps['customParams']> = {
         asset: {
-          object: {
-            asset: 'Samba Dancing'
-          },
+          object: selectObject,
           prop: 'asset',
           control: {
             label: 'Asset',
@@ -93,15 +94,15 @@ describe('Custom Params', () => {
         const key_1Input = key_1.parentElement!.querySelector('input');
         key_1Input!.value = '0.5';
         key_1Input!.dispatchEvent(new Event('change'));
-        expect(obj.val1).toBe(0.5);
+        expect(morphObject.val1).toBe(0.5);
         const key_2 = await screen.findByText('Key 2');
         const key_2Input = key_2.parentElement!.querySelector('input');
         key_2Input!.value = '0.6';
         key_2Input!.dispatchEvent(new Event('change'));
-        expect(obj.val2).toBe(0.6);
+        expect(morphObject.val2).toBe(0.6);
         expect(handleValueChange).toHaveBeenCalledTimes(2);
         // prettier-ignore
-        expect(handleValueChange.mock.calls).toEqual([[0.5, obj, 'val1'], [0.6, obj, 'val2']]);
+        expect(handleValueChange.mock.calls).toEqual([[0.5, morphObject, 'val1'], [0.6, morphObject, 'val2']]);
         expect(Object.keys(cParams)).toEqual(['asset', 'morph']);
         // switch back to 'Samba Dancing'
         // @ts-ignore
@@ -120,21 +121,9 @@ describe('Custom Params', () => {
         expect(Object.keys(cParams)).toEqual([]);
         // prettier-ignore
         expect(spyOnSetOrUpdateCPanelCustomParams.mock.calls).toEqual([
-          [
-            'asset', { asset: 'Samba Dancing' }, 'asset',
-            { label: 'Asset', options: { 'Samba Dancing': 'Samba Dancing', morph_test: 'morph_test' }, onChange: handleCParamChange, picker: 'inline' },
-            []
-          ],
-          [
-            'key_1', { val1: 0.5, val2: 0.6 }, 'val1',
-            { label: 'Key 1', min: 0, max: 1, onChange: handleValueChange, picker: 'inline' },
-            ['morph', 'nested', 'level']
-          ],
-          [
-            'key_2', { val1: 0.5, val2: 0.6 }, 'val2',
-            { label: 'Key 2', min: 0, max: 1, onChange: handleValueChange, picker: 'inline' },
-            ['morph', 'nested', 'level']
-          ]
+          ['asset', selectObject, customParams.asset.prop, customParams.asset.control, []],
+          ['key_1', morphObject, customParamsMorph.nested.level.key_1.prop, customParamsMorph.nested.level.key_1.control, ['morph', 'nested', 'level']],
+          ['key_2', morphObject, customParamsMorph.nested.level.key_2.prop, customParamsMorph.nested.level.key_2.control, ['morph', 'nested', 'level']]
         ]);
         expect(spyOnRemoveCPanelCustomParams.mock.calls).toEqual([
           ['key_1', ['morph', 'nested', 'level']],

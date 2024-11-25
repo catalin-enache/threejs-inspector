e0206aebdae6453c1ea26a9ba735a1fcb7f0c7a5
Catalin Enache <catalinenache78@gmail.com>
2024-11-25 12:44:46 +0200

    added native app test

---
+++ src/tests/NativeApp.test.ts
@@ -0,0 +1,55 @@
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { waitFor } from '@testing-library/dom';
+import { defaultOrthographicCamera, defaultPerspectiveCamera, defaultScene } from 'lib/App/SetUp/patchThree';
+import { useAppStore } from 'src/store';
+import { CPanelProps } from 'lib/App/CPanel/CPanel';
+import { initNativeApp } from 'testutils/testNativeApp';
+import { SETUP_EFFECT, SetUpProps } from 'lib/App/SetUp/SetUp';
+
+describe('injectInspector', () => {
+  beforeEach(() => {});
+
+  afterEach(() => {
+    defaultScene.clear();
+    useAppStore.getState().reset();
+  });
+
+  it('injects Inspector and provides updateInspector and unmountInspector', { timeout: 1000 }, async () => {
+    return new Promise<void>((done) => {
+      const handleSetupEffect: SetUpProps['onSetupEffect'] = async (effect, data) => {
+        if (effect === SETUP_EFFECT.VERSION_CHANGED) {
+          if (data.version === 1) {
+            setTimeout(() => {
+              updateInspector({
+                camera: defaultOrthographicCamera
+              });
+            }, 0);
+            await waitFor(() => expect(scene.__inspectorData.currentCamera).toBe(defaultOrthographicCamera));
+          } else if (data.version === 2) {
+            setTimeout(() => {
+              updateInspector({
+                camera: defaultPerspectiveCamera
+              });
+            }, 0);
+            await waitFor(() => expect(scene.__inspectorData.currentCamera).toBe(defaultPerspectiveCamera));
+          } else if (data.version === 3) {
+            setTimeout(() => {
+              unmountInspector();
+            }, 0);
+          }
+        }
+      };
+
+      const handleCPanelUnmounted: CPanelProps['onCPanelUnmounted'] = () => {
+        cleanUp();
+        done();
+      };
+
+      const { cleanUp, updateInspector, unmountInspector, scene } = initNativeApp({
+        autoNavControls: true,
+        onCPanelUnmounted: handleCPanelUnmounted,
+        onSetupEffect: handleSetupEffect
+      });
+    });
+  });
+});

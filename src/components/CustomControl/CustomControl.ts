b93ad1a96a40478a2c3a009d302bce226ba36419
Catalin Enache <catalinenache78@gmail.com>
2024-11-27 18:47:04 +0200

    updated CustomControl

--- src/components/CustomControl/CustomControl.ts
+++ src/components/CustomControl/CustomControl.ts
@@ -6,25 +6,24 @@ export interface CustomControlProps {
   name: string;
   object?: Record<string, any>;
   prop?: string;
-  control: BindingParams;
+  control: BindingParams & { onChange?: (value: any) => void; onClick?: (value: any) => void };
   path?: string;
 }
 
 export const CustomControl = (props: CustomControlProps) => {
-  const { name, prop, control, path = '', object } = props;
-  const setOrUpdateCPanelCustomParams = useAppStore((state) => state.setOrUpdateCPanelCustomParams);
-  const removeCPanelCustomParams = useAppStore((state) => state.removeCPanelCustomParams);
-  const triggerCPanelCustomParamsStructureChanged = useAppStore(
-    (state) => state.triggerCPanelCustomParamsStructureChanged
-  );
-
   useEffect(() => {
+    const { name, prop, control, path = '', object } = props;
     const pathArray = path
       .split('/')
       .map((p) => p.trim())
       .filter((p) => p.length > 0);
+    const setOrUpdateCPanelCustomParams = useAppStore.getState().setOrUpdateCPanelCustomParams;
+    const removeCPanelCustomParams = useAppStore.getState().removeCPanelCustomParams;
+    const triggerCPanelCustomParamsStructureChanged = useAppStore.getState().triggerCPanelCustomParamsStructureChanged;
+
     setOrUpdateCPanelCustomParams(name, object, prop, control, pathArray);
     triggerCPanelCustomParamsStructureChanged();
+
     return () => {
       removeCPanelCustomParams(name, pathArray);
       triggerCPanelCustomParamsStructureChanged();

7048013489dc245cc38a1057d93c09e892eec5d4
Catalin Enache <catalinenache78@gmail.com>
2025-01-15 13:09:47 +0200

    added new loadModel test

--- src/tests/lib/utils/loadModel.test.ts
+++ src/tests/lib/utils/loadModel.test.ts
@@ -21,6 +21,39 @@ describe('loadModel', () => {
 
         scene.add(fbx);
 
+        // using LoadingManager.onLoad to wait after all textures have been loaded
+        const onLoad = () => {
+          const isOK = fbx.children.every((child) => {
+            return ((child as THREE.Mesh).material as THREE.MeshPhongMaterial).map?.source.data.width === 512;
+          });
+          expect(isOK).toBeTruthy();
+          window.removeEventListener('LoadingManager.onLoad', onLoad);
+        };
+
+        window.addEventListener('LoadingManager.onLoad', onLoad);
+        // await new Promise((resolve) => setTimeout(resolve, 60000));
+      })
+    );
+  });
+
+  describe('when path has spaces', () => {
+    it('loads model correctly', { timeout: 5000 }, async () =>
+      withScene()(async ({ scene, camera }) => {
+        const fbx = await loadModel('asset with space in path.FBX', {
+          autoScaleRatio: 0.01,
+          scene,
+          camera,
+          path: '/models/MyTests/having space in path/'
+        });
+        console.log(fbx);
+
+        if (!fbx) {
+          throw new Error('Failed to load model');
+        }
+
+        scene.add(fbx);
+
+        // using LoadingManager.onLoad to wait after all textures have been loaded
         const onLoad = () => {
           const isOK = fbx.children.every((child) => {
             return ((child as THREE.Mesh).material as THREE.MeshPhongMaterial).map?.source.data.width === 512;

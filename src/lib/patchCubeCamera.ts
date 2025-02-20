b9b21daff2a2948b4fe450ecee730946486d59c0
Catalin Enache <catalinenache78@gmail.com>
2025-02-20 15:42:25 +0200

    patch CubeCamera and RenderTarget

---
+++ src/lib/patchCubeCamera.ts
@@ -0,0 +1,21 @@
+import * as THREE from 'three';
+import 'src/lib/patchRenderTarget';
+import type { CubeCameraAsJson } from 'tsExtensions';
+
+THREE.CubeCamera.prototype.toJSON = (function() {
+  return function(this: THREE.CubeCamera, meta?: THREE.JSONMeta): CubeCameraAsJson {
+    const children = [...this.children];
+    // removing internal cameras
+    while (this.children.length) {
+      this.remove(this.children[0]);
+    }
+    const _data: THREE.Object3DJSON = THREE.Object3D.prototype.toJSON.call(this, meta);
+    const data = _data as CubeCameraAsJson;
+    const { near, far } = children[0] as THREE.PerspectiveCamera;
+    data.object.near = near;
+    data.object.far = far;
+    data.object.renderTarget = this.renderTarget.toJSON(meta);
+    this.children = children;
+    return data;
+  };
+})();
---
+++ src/lib/patchRenderTarget.ts
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+import type { RenderTargetAsJson } from 'tsExtensions';
+
+THREE.RenderTarget.prototype.toJSON = (function() {
+  return function(this: THREE.RenderTarget, meta?: THREE.JSONMeta): RenderTargetAsJson {
+    const data: RenderTargetAsJson = {
+      width: this.width,
+      height: this.height,
+      options: {
+        wrapS: this.texture.wrapS,
+        wrapT: this.texture.wrapT,
+        magFilter: this.texture.magFilter,
+        minFilter: this.texture.minFilter,
+        generateMipmaps: this.texture.generateMipmaps,
+        format: this.texture.format,
+        type: this.texture.type,
+        anisotropy: this.texture.anisotropy,
+        colorSpace: this.texture.colorSpace,
+        internalFormat: this.texture.internalFormat,
+        depthBuffer: this.depthBuffer,
+        stencilBuffer: this.stencilBuffer,
+        resolveDepthBuffer: this.resolveDepthBuffer,
+        resolveStencilBuffer: this.resolveStencilBuffer,
+        depthTexture: this.depthTexture && {
+          ...this.depthTexture.toJSON(meta),
+          imageWidth: this.depthTexture.image.width,
+          imageHeight: this.depthTexture.image.height
+        },
+        samples: this.samples,
+        count: this instanceof THREE.WebGLCubeRenderTarget ? 1 : this.texture.image.length
+      }
+    };
+    return data;
+  };
+})();

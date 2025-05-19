443feab02a42f6ec51cb2e5fb20e6a4aa01d4645
Catalin Enache <catalinenache78@gmail.com>
2025-05-19 13:29:18 +0300

    old_src removed

--- old_src/index.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import { ScenarioSelect } from 'old_src/components/ControlPanel';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('controlPanel')!).render(
-  // <React.StrictMode>
-  <ScenarioSelect />
-  // </React.StrictMode>
-);

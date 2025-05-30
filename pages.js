323cf3c67a0d452b86580f2ca494deb670d27d91
Catalin Enache <catalinenache78@gmail.com>
2025-05-30 18:38:58 +0300

    Updates

---
+++ pages.js
@@ -0,0 +1,14 @@
+export const pages = [
+  {
+    title: 'Index',
+    page: 'index.html'
+  },
+  {
+    title: 'Index Injected',
+    page: 'index_injected.html'
+  },
+  {
+    title: 'Index Injected Native',
+    page: 'index_injected_native.html'
+  }
+];
